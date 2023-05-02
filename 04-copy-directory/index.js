5303d793c01ac70b2b00b387dab2d4d89b476cf8
Eugene Solomonyk <solosoft2000@mail.ru>
2023-05-02 20:28:19 +0300

    feat: start 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir(pathFrom, pathTo) {
+  console.log(pathFrom, pathTo);
+}
+
+copyDir(path.join(__dirname), path.join(__dirname, '-copy'));
