b83550b0c3ba04f1df272f404dd21a6f1b337881
Eugene Solomonyk <solosoft2000@mail.ru>
2023-05-03 17:55:47 +0300

    feat: work with 06

---
+++ 06-build-page/index.js
@@ -0,0 +1,28 @@
+const fsPromises = require('node:fs/promises');
+//const fs = require('node:fs');
+const path = require('node:path');
+const mergeCss = require('./mergeCss.js');
+const copyDir = require('./copyDir.js');
+
+const distPath = 'project-dist';
+
+async function changeTemplate(array, htmlText) {
+  for (const template of array) {
+    const txt = await fsPromises.readFile(path.join(__dirname, 'components', `${template[1]}.html`));
+    htmlText = htmlText.replace(template[0], txt.toString());
+  }
+  console.log(htmlText);
+}
+
+
+fsPromises.mkdir(path.join(__dirname, distPath), { recursive: true }).then(() => { // создаем папку
+  mergeCss.mergeCss(distPath, 'style.css'); // объединяем css
+  copyDir.copyDir(path.join(__dirname, 'assets'), path.join(__dirname, distPath, 'assets')); // копируем папку assets
+});
+
+
+fsPromises.readFile(path.join(__dirname, 'template.html')).then((data)=>{
+  let tmpHtml = data.toString();
+  const tempMatchArr = Array.from(tmpHtml.matchAll(/{{(.*?)}}/g));
+  changeTemplate(tempMatchArr, tmpHtml);
+});
