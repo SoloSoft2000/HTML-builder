d280e8b5b2d679e3765fc2231980209409610a3b
Eugene Solomonyk <solosoft2000@mail.ru>
2023-05-06 07:56:33 +0300

    fix: 04 - with promises

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,23 @@
 const fsPromises = require('node:fs/promises');
-const fs = require('fs');
 const path = require('path');
 
-function copyDir(pathFrom, pathTo) {
-  fsPromises.rm(pathTo, {force: true, recursive: true}).then(() => {
-    fs.mkdir(pathTo, { recursive: true }, (err) => {
-      if (err) throw err;
-      fs.readdir(pathFrom, {withFileTypes: true}, (error, dirList) => {
-        dirList.forEach((fileName) => {
-          fs.copyFile(path.join(pathFrom, fileName.name), path.join(pathTo, fileName.name), (err) => {
-            if (err) throw err;
-          });
-        });
-      });
-    });
-  });
+const copyDir = (pathFrom, pathTo) => {
+  fsPromises.rm(pathTo, {force: true, recursive: true})
+  .then(() => {
+    fsPromises.mkdir(pathTo, { recursive: true })
+    .then(() => {
+      fsPromises.readdir(pathFrom, {withFileTypes: true})
+      .then((dirList) => {
+        for (const item of dirList) {
+          if (item.isDirectory()) {
+            copyDir(path.join(pathFrom, item.name), path.join(pathTo, item.name));
+          } else {
+            fsPromises.copyFile(path.join(pathFrom, item.name), path.join(pathTo, item.name));
+          }
+        }
+      })
+    })
+  })
 }
 
 copyDir(path.join(__dirname,'files'), path.join(__dirname, 'files-copy'));
